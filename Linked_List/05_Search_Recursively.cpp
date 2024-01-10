c9b317099fb77b2f6df4c53a3493482a5300df02
JbBhetwal <jbjbjbaaa@gmail.com>
2024-01-10 07:56:03 +0545

    Search

--- Linked_List/05_Search_Recursively.cpp
+++ Linked_List/05_Search_Recursively.cpp
@@ -46,38 +46,49 @@ void printList(Node *head)
         cout << curr->data << " ";
         curr = curr->next;
     }
+    cout << endl;
 }
 
 int search(Node *head, int x)
 {
-    int pos = 1;
+    int pos;
     Node *curr = head;
-    while (curr != NULL)
+    if (curr == NULL)
     {
-        if (curr->data != x)
-        {
-            curr = curr->next;
-            pos++;
-        }
-        return pos;
+        return -1;
     }
-    return -1;
+    if (curr->data == x)
+    {
+        return 1;
+    }
+    else
+    {
+        pos = search(curr->next, x);
+        if (pos == -1)
+        {
+            return -1;
+        }
+        else
+        {
+            return pos + 1;
+        }
+    }
 }
 
 int main()
 {
     Node *head = NULL;
-    // head = insertBeg(head, 10);
-    // head = insertBeg(head, 20);
-    // head = insertEnd(head, 30);
-    // head = insertEnd(head, 40);
-    // head = insertEnd(head, 50);
-    // head = insertEnd(head, 90);
-    // head = insertEnd(head, 0);
-    // head = insertEnd(head, 80);
+    head = insertBeg(head, 10);
+    head = insertBeg(head, 20);
+    head = insertEnd(head, 30);
+    head = insertEnd(head, 40);
+    head = insertEnd(head, 50);
+    head = insertEnd(head, 90);
+    head = insertEnd(head, 0);
+    head = insertEnd(head, 80);
 
-    // printList(head);
+    printList(head);
 
-    cout << search(head, 900);
+    cout << search(head, 40);
     return 0;
 }
